d9b97e26a52bd9abd077e3b51dc0de0859717d1e
abhishek <abhi.l1225@gmail.com>
2020-10-25 08:50:49 +0530

    UI Changes

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Card, Input, Form, Button} from "antd";
+import './style.css';
 
 function Login(props) {
     
@@ -9,33 +10,45 @@ function Login(props) {
         window.location = "/home"
     }
     return (
-        <Card title={"Login"}>
-            <Form
-                name="basic"
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
+        <div className={'loginwrapper'}>
+            <Card title={"Login"} className={"login"}>
+                <Form
+                    layout={'horizontal'}
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
                 >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                    Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-        </Card>
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                            { required: true, message: 'Please input your username!' },
+                            {min:4, message:"Minimum 4 characters required."},
+                            {max:10, message:"Maximum 10 characters required."}
+                    ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            { required: true, message: 'Please input your password!' },
+                            {min:4, message:"Minimum 4 characters required."},
+                            {max:10, message:"Maximum 10 characters required."},
+                        
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" style={{float:"right", width:"100%"}}>
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
     )
 }
 
